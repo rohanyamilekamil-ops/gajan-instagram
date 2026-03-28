83565e9fc8402bf54643842369e8b6c278d6e67f
rohanyamilekamil-ops <rohanyamilekamil@gmail.com>
2026-03-29 04:50:30 +0530

    Update  LikeSystem.js

--- pages/ LikeSystem.js
+++ pages/ LikeSystem.js
@@ -1,18 +1,11 @@
-import { db } from './firebaseConfig';
+import { db } from '../firebaseConfig';
 import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
 
 export const handleLike = async (postId, userId, hasLiked) => {
   const postRef = doc(db, "posts", postId);
-  
   if (hasLiked) {
-    // Agar pehle se like hai to remove karo
-    await updateDoc(postRef, {
-      likes: arrayRemove(userId)
-    });
+    await updateDoc(postRef, { likes: arrayRemove(userId) });
   } else {
-    // Naya like add karo
-    await updateDoc(postRef, {
-      likes: arrayUnion(userId)
-    });
+    await updateDoc(postRef, { likes: arrayUnion(userId) });
   }
 };
