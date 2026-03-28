a1cdaf7f4834248e965f0774016d2ae0e14b485b
rohanyamilekamil-ops <rohanyamilekamil@gmail.com>
2026-03-29 02:58:46 +0530

    Update AdminController.js

--- pages/AdminController.js
+++ pages/AdminController.js
@@ -1,1 +1,18 @@
 
+import { db } from './firebaseConfig';
+import { doc, updateDoc, deleteDoc } from "firebase/firestore";
+
+export const ADMIN_CREDENTIALS = {
+  user: "MRGAJAN",
+  pass: "Abdul6353738585@"
+};
+
+// Admin Power: QR Code Change
+export const changeQR = async (newUrl) => {
+  await updateDoc(doc(db, "appConfig", "payments"), { qrCodeUrl: newUrl });
+};
+
+// Admin Power: 10 Million Tokens Stock
+export const setAdminStock = async () => {
+  await updateDoc(doc(db, "users", "MRGAJAN_UID"), { tokens: 10000000 });
+};
