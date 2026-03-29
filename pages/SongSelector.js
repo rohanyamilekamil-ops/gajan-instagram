7285d00d5f9eac267eeceb1a55f469ee881cb6c3
rohanyamilekamil-ops <rohanyamilekamil@gmail.com>
2026-03-29 20:19:56 +0530

    Delete pages/SongSelector.js

--- pages/SongSelector.js
+++
@@ -1,5 +0,0 @@
-export const trendingSongs = [
-  { id: 1, name: "Maula Ali Qawwali", url: "link_to_song_1" },
-  { id: 2, name: "Instagram Viral Beat", url: "link_to_song_2" },
-  { id: 3, name: "Jamnagar Vibes", url: "link_to_song_3" }
-];
